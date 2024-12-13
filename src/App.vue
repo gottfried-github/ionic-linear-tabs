765af82687e9aa292b2ad47155dc0a9f0b314e75
gottfried <gottfried.dev@gmail.com>
2024-12-13 17:14:55 +0200

    add navigation components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
 <template>
   <ion-app>
-    <ion-router-outlet />
+    <ion-menu content-id="main-content" type="overlay">
+      <ion-content>
+        <ion-list id="inbox-list">
+          <ion-list-header>Triyoga</ion-list-header>
+          <ion-note>michael@gmail.com</ion-note>
+
+          <ion-menu-toggle :auto-hide="false">
+            <ion-item
+              router-direction="root"
+              router-link="/home"
+              lines="none"
+              :detail="false"
+              class="hydrated"
+            >
+              <ion-label>Home</ion-label>
+            </ion-item>
+          </ion-menu-toggle>
+        </ion-list>
+      </ion-content>
+    </ion-menu>
+    <ion-router-outlet id="main-content" />
+    <ion-footer class="main-footer">
+      <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-button router-link="/home">Home</ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-footer>
   </ion-app>
 </template>
 
+<style scoped>
+.main-footer {
+  position: fixed;
+  bottom: 0px;
+}
+</style>
+
 <script setup lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonMenu,
+  IonMenuToggle,
+  IonNote,
+  IonFooter,
+  IonToolbar,
+  IonButton,
+} from '@ionic/vue'
 </script>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,10 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
 
@@ -15,20 +18,28 @@
 
       <div id="container">
         <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue'
 </script>
 
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -44,9 +55,9 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
